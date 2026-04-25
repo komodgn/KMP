06b064077e249cfa4fb9201423786d0bdd130ecd
komodgn <a01075251881@gmail.com>
2026-04-25 23:34:28 +0900

    chore: Bump version to 0.2.0

--- calendar-ui/build.gradle.kts
+++ calendar-ui/build.gradle.kts
@@ -130,7 +130,7 @@ publishing {
     publications {
         withType<MavenPublication> {
             groupId = "io.github.komodgn"
-            version = "0.1.7"
+            version = "0.2.0"
         }
     }
 }
