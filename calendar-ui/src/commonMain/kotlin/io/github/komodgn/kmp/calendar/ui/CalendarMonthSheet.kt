02ec40f15c740250dd8b178eacaaac2f83015e53
komodgn <a01075251881@gmail.com>
2026-04-27 16:00:44 +0900

    style: Apply spotless

--- calendar-ui/src/commonMain/kotlin/io/github/komodgn/kmp/calendar/ui/CalendarMonthSheet.kt
+++ calendar-ui/src/commonMain/kotlin/io/github/komodgn/kmp/calendar/ui/CalendarMonthSheet.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2026 komodgn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.komodgn.kmp.calendar.ui
 
 import androidx.compose.foundation.background
@@ -28,7 +43,7 @@ fun CalendarMonthSheet(
 ) {
     LazyVerticalGrid(
         columns = GridCells.Fixed(7),
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier.fillMaxWidth(),
     ) {
         items(days) { date ->
             val isSelected = date != null && date == selectedDate
