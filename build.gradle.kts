4970d0a42a151d543433d7199b96dd3767cd66d7
komodgn <a01075251881@gmail.com>
2026-04-27 15:56:27 +0900

    chore: Setup spotless and copyright header

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,4 +8,35 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.androidKotlinMultiplatformLibrary) apply false
     alias(libs.plugins.androidLint) apply false
+    alias(libs.plugins.spotless) apply false
+}
+
+subprojects {
+    apply(plugin = "com.diffplug.spotless")
+
+    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+        kotlin {
+            target("**/*.kt")
+            targetExclude("**/build/**/*.kt")
+
+            licenseHeaderFile(rootProject.file("spotless/copyright.kt"))
+
+            ktlint("1.8.0")
+                .editorConfigOverride(
+                    mapOf(
+                        "ktlint_function_naming_ignore_when_annotated_with" to "Composable",
+                        "ktlint_standard_function-naming" to "disabled",
+                    )
+                )
+            trimTrailingWhitespace()
+            endWithNewline()
+        }
+        format("kts") {
+            target("**/*.kts")
+            targetExclude("**/build/**/*.kts")
+            licenseHeaderFile(rootProject.file("spotless/copyright.kt"), "(^(?![\\/ ]\\*).*$)")
+            trimTrailingWhitespace()
+            endWithNewline()
+        }
+    }
 }
